eddf10ac2361cc3e1d595e535e96707dbecbf39b
saintbassanaga <saintbassanaga01@gmail.com>
2024-05-11 09:26:04 +0100

    fix: Adding transaction support

--- src/main/java/tech/saintbassanaga/liveasy_test/LiveasyTestApplication.java
+++ src/main/java/tech/saintbassanaga/liveasy_test/LiveasyTestApplication.java
@@ -2,8 +2,10 @@ package tech.saintbassanaga.liveasy_test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class LiveasyTestApplication {
 
     public static void main(String[] args) {
